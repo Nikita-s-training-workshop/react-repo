2f3b31b4a04f6408c3435e16a6b539c0b55c379c
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2024-09-27 21:40:42 +0500

    I studied the managed inputs (#1)

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react'
 
 function App() {
   const [likes, setLikes] = useState(5)
+  const [inputValue, setInputValue] = useState("Текст в инпуте")
   
-  function increment () {
-    setLikes(likes + 1)
-  }
-  
-  function decrement () {
-    setLikes(likes - 1)
-  }
-  
-  
-  
+  const increment = () => setLikes(likes + 1)
+  const decrement = () => setLikes(likes - 1)
+
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h2>{inputValue}</h2>
+      <input
+        type = "text"
+        value={value}
+        onChange={event => setInputValue(event.target.value)}
+      />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>

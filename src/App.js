8908d3dd11e546e01d4b5ba7f17aa7db891df123
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2024-09-27 15:36:22 +0500

    minor refactoring

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ function App() {
   const [likes, setLikes] = useState(5)
   const [value, setValue] = useState("Текст в инпуте")
   
-  function increment () {
-    setLikes(likes + 1)
-  }
-  
-  function decrement () {
-    setLikes(likes - 1)
-  }
-  
-  
-  
+  const increment = () => {setLikes(likes + 1)}
+  const decrement = () => {setLikes(likes - 1)}
+
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <h1>{value}</h1>
+      <h2>{value}</h2>
       <input
         type = "text"
         value={value}

38a52442917eee95e73ce32af9008f587c831d22
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2024-09-27 18:03:55 +0500

    change new value name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 function App() {
   const [likes, setLikes] = useState(5)
-  const [value, setValue] = useState("Текст в инпуте")
+  const [inputValue, inputSetValue] = useState("Текст в инпуте")
   
   const increment = () => {setLikes(likes + 1)}
   const decrement = () => {setLikes(likes - 1)}
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <h2>{value}</h2>
+      <h2>{inputValue}</h2>
       <input
         type = "text"
         value={value}
-        onChange={event => setValue(event.target.value)}
+        onChange={event => inputSetValue(event.target.value)}
       />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>

46fc4d63e29f6c8db950074fa1b6fd6ce16b88e7
nnnikserrrr <nnnikserrrr@icloud.com>
2024-09-30 02:02:46 +0400

    add button and input styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './styles/App.css'
 import {PostList} from './components/PostList'
+import {MyButton} from './components/UI/button/MyButton'
+import {MyInput} from './components/UI/input/MyInput'
 
 const INITIAL_STATE = [
   {id:1, title: 'JavaScript', body:'Description'},
@@ -11,13 +13,16 @@ const INITIAL_STATE = [
 export const App = () => {
   
   const [posts, setPosts] = useState(INITIAL_STATE)
+  const addNewPost = () => {
+  
+  }
   
   return (
     <div className="App">
       <form>
-        <input type='text' placeholder='Название поста' />
-        <input type='text' placeholder='Описание поста' />
-        <button>Создать пост</button>
+        <MyInput type='text' placeholder='Название поста' />
+        <MyInput type='text' placeholder='Описание поста' />
+        <MyButton onClick = {addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title="Список постов 1"/>
     </div>
